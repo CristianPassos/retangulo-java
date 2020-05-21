d76277cb4c8b2929ba30c71958ae72a65edce90d
CristianPassos <cristianpassosdasilva@gmail.com>
2020-05-21 08:38:22 -0300

    Programa finalizado

--- src/app/App.java
+++ src/app/App.java
@@ -23,6 +23,40 @@ public class App {
 		while (altRetangulo <= 0) {
 			System.out.println("O valor deve ser positivo. Tente novamente: ");
 			altRetangulo = sc.nextDouble();
+        }
+        while (opcao < 3) {
+			System.out.println();
+			System.out.println("MENU:");
+			System.out.println("1 - Mostrar area");
+			System.out.println("2 - Mostrar perimetro");
+			System.out.println("3 - Sair");
+			System.out.println("Digite uma opcao: ");
+			opcao = sc.nextInt();
+			while (opcao > 3 || opcao < 1) {
+				System.out.println();
+				System.out.println("OPÇÃO INVALIDA");
+
+				System.out.println();
+				System.out.println("MENU:");
+				System.out.println("1 - Mostrar area");
+				System.out.println("2 - Mostrar perimetro");
+				System.out.println("3 - Sair");
+				System.out.println("Digite uma opcao: ");
+				opcao = sc.nextInt();
+			}
+
+			System.out.println();
+
+			if (opcao == 1) {
+				areaRetangulo = largRetangulo * altRetangulo;
+				System.out.println("AREA = " + areaRetangulo);
+			} else if (opcao == 2) {
+				perimeRetangulo = 2 *(largRetangulo + altRetangulo);
+				System.out.println("PERIMETRO = " + perimeRetangulo);
+			} else {
+				System.out.println("FIM DO PROGRAMA!");
+			}
+
 		}
 
         sc.close();
